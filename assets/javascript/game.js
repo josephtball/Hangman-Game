264ca235dc29d33c788f361d1326a996c8233568
Joseph Ball <jball321@live.com>
2017-04-11 22:26:38 -0400

    Added images and sounds to game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,18 +2,34 @@
 var wins = 0;
 var losses = 0;
 var word = '';
-
+var image = '';
 
 function coreGame() {
 
 	var letterGuesses = [];
 	var wrongLetters = [];
 	var userGuess = "";
+
+	// reset wins/losses
+	wins = 0;
+	document.querySelector('#wins').innerHTML = wins;
+	losses = 0;
+	document.querySelector('#losses').innerHTML = losses;
+
+	// reset image
+	document.querySelector('#image').setAttribute('src', 'assets/images/question-mark.png');
 	
 	// randomly selecting a word
-	var words = ['HONDA', 'MERCEDES', 'FORD', 'FERRARI', 'VOLKSWAGON', 'CADILLAC'];
-	word = words[Math.floor(Math.random() * words.length)];
+	var options = {
+		words: ['HONDA', 'MERCEDES', 'FORD', 'FERRARI', 'VOLKSWAGON', 'CADILLAC'],
+		images: ['assets/images/honda.png', 'assets/images/mercedes.png', 'assets/images/ford.png', 'assets/images/ferrari.png', 'assets/images/volkswagon.png', 'assets/images/cadillac.png'],
+	}
+	word = options.words[Math.floor(Math.random() * options.words.length)];
 	var wordArr = word.split('');
+
+	// get image file
+	var index = options.words.indexOf(word);
+	image = options.images[index];
 
 	// write "_ _ _" to screen
 	var hiddenWordArr = [];
@@ -23,7 +39,7 @@ function coreGame() {
 	document.querySelector('#word').innerHTML = hiddenWordArr.join(' ');
 
 	// set number of guesses
-	var remainingGuesses = 12;
+	var remainingGuesses = 6;
 	document.querySelector('#remainingGuesses').innerHTML = remainingGuesses;
 
 	// reset wrong letter guesses
@@ -60,14 +76,20 @@ if (remainingGuesses > 0) {
 		if (remainingGuesses == 0) {
 			losses++;
 			document.querySelector('#losses').innerHTML = losses;
-			setTimeout(lose, 50);
+			document.getElementById('loseAudio').play();
+			document.querySelector('#image').setAttribute('src', image);
+
+			setTimeout(lose, 500);
 		}
 
 		// if user wins
 		if (hiddenWordArr.join() == wordArr.join()) {
+			remainingGuesses = 0;
 			wins++;
 			document.querySelector('#wins').innerHTML = wins;
-			setTimeout(won, 50);
+			document.getElementById('winAudio').play();
+			document.querySelector('#image').setAttribute('src', image);
+			setTimeout(won, 500);
 		}
 }
 	} // end get user's guess function
